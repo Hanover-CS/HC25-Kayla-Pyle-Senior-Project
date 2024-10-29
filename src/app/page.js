8c518802d9d9e964e2cb71c1b29d8f178985e770
Kayla Pyle <pylek25@hanover.edu>
2024-10-29 15:00:41 -0400

    added image to home page ref: Home Page #10

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
+import worldMap from "./assets/WorldMap.jpg"
 
 export default function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-8 row-start-2 items-center">
         <h1 className="text-4xl font-serif font-bold">MyWorld Wiki</h1>
+        <Image  className="" src={worldMap} alt="WorldMap" width={400} height={400}/>
       </main>
       <div className="absolute top-0 right-0 p-8">
         <Link href="/dashboard">
