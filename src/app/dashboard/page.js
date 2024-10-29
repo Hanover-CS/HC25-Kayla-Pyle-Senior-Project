ac69532bf3938256ed373ce363f94698b6e52777
Kayla Pyle <pylek25@hanover.edu>
2024-10-29 15:02:29 -0400

    add database connection to the dashboard page file ref: Set up Neo4j Connection #9

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,6 +1,33 @@
 
-export default function Dashboard() {
+(async () => {
 
+    var neo4j = require('neo4j-driver')
+  
+    const URI = 'neo4j+s://b0362c08.databases.neo4j.io'
+    const USER = 'neo4j'
+    const PASSWORD = 'EY67C9MSuqJjbU-ZarxV40Eakor7UnuHiNGAKuXlgig'
+    let driver
+  
+    try {
+      driver = neo4j.driver(URI,  neo4j.auth.basic(USER, PASSWORD))
+      const serverInfo = await driver.getServerInfo()
+      console.log('Connection established')
+      console.log(serverInfo)
+    } catch(err) {
+      console.log(`Connection error\n${err}\nCause: ${err.cause}`)
+      await driver.close()
+      return
+    }
+  
+    // Use the driver to run queries
+    
+  
+    await driver.close()
+
+  })();
+
+
+export default function Dashboard() {
     return(
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
