72e96caba2a17c12ee905594c00ee269f0a5c7f9
Kayla Pyle <pylek25@hanover.edu>
2024-10-31 22:39:56 -0400

    move database connection to it's own file ref: Set up Neo4j Connection #9

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,39 +1,19 @@
+import { read } from '../lib/neo4j.js'
 
-(async () => {
+async function populateSelect() {
+  const entries = await read('MATCH (e:Entry) RETURN e.name AS name', {})
+  console.log(entries);
+}
 
-    var neo4j = require('neo4j-driver')
-  
-    const URI = process.env.URI
-    const USER = process.env.USER
-    const PASSWORD = process.env.PASSWORD
-    let driver
-  
-    try {
-      driver = neo4j.driver(URI,  neo4j.auth.basic(USER, PASSWORD))
-      const serverInfo = await driver.getServerInfo()
-      console.log('Connection established')
-      console.log(serverInfo)
-    } catch(err) {
-      console.log(`Connection error\n${err}\nCause: ${err.cause}`)
-      await driver.close()
-      return
-    }
-  
-    // Use the driver to run queries
-    
-  
-    await driver.close()
-
-  })();
-
+populateSelect()
 
 export default function Dashboard() {
-    return(
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-                <h1 className="text-4xl font-serif font-bold">Dashboard Page</h1>
-            </main>
-        </div>
+  return(
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <h1 className="text-4xl font-serif font-bold">Dashboard Page</h1>
+      </main>
+    </div>
     );
 
 }
