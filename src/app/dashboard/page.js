7235be8d4f8347a9ed537b9e30feebaf08e8b500
Kayla Pyle <pylek25@hanover.edu>
2024-11-14 21:01:44 -0500

    add database-based select component to dashboard page Dashboard #11

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,17 +1,31 @@
 import { read } from '../lib/neo4j.js'
+import React from 'react'
 
-async function populateSelect() {
-  const entries = await read('MATCH (e:Entry) RETURN e.name AS name')
-  console.log(entries)
+const entries = await read('MATCH (e:Entry) RETURN e.name AS name')
+
+
+function DashboardSelect(entries) {
+  return (
+    <select>
+      {entries.items.map((item, index) => (
+        <option key={index}>{item}</option>
+      ))}
+    </select>
+  )
 }
 
-populateSelect()
-
 export default function Dashboard() {
+
+  const entriesList = []
+  for (let i = 0; i < entries.length; i++) {
+    entriesList.push(entries[i].name)
+  }
+
   return(
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1 className="text-4xl font-serif font-bold">Dashboard Page</h1>
+        <DashboardSelect items={entriesList}/>
       </main>
     </div>
     );
