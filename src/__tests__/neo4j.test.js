4251c03cc30cfff5757084b036cb2744366c9559
Kayla Pyle <pylek25@hanover.edu>
2024-11-14 22:45:31 -0500

    make three unit tests for tests in neo4j.js Create Unit Tests #16

--- src/__tests__/neo4j.test.js
+++ src/__tests__/neo4j.test.js
@@ -2,12 +2,39 @@
 // Testing the write, read, 
 
 import { expect, test } from 'vitest'
-import { read, write } from '../app/lib/neo4j'
+import { createEntry, readEntry, getAllEntries, deleteEntry } from '../app/lib/neo4j'
 
+function clearDB() {
+    const entries = getAllEntries()
+    for (let i = 0; i < entries.length; i++) {
+        deleteEntry(entries[i].name)
+    }
+}
 
-
-test('write method', async () => {
-    await write("MERGE (:Entry:Event {name: 'Jousting Tournament', text:''})")
-
-    // expect(1 + 2).toBe(3)
+test('createEntry method adds Bob', async () => {
+    await createEntry('Bob')
+    let result = await readEntry('Bob')
+    expect(result).toEqual([{name: 'Bob'}])
+    clearDB()
 })
+
+test('deleteEntry deletes Bob', async () => {
+    await createEntry('Bob')
+    deleteEntry('Bob')
+    let result = await readEntry('Bob')
+    expect(result).toEqual([])
+})
+
+test('getAllEntries returns Bob, Sally, and Monica', async () => {
+    await createEntry('Bob')
+    await createEntry('Sally')
+    await createEntry('Monica')
+    let result = await getAllEntries()
+    expect(result).toEqual([{name: 'Bob'}, {name: 'Sally'}, {name: 'Monica'}])
+    clearDB()
+})
+
+// test('readEntry method does not read an entry that is not there', async () => {
+//     let result = await readEntry('Bob')
+//     expect(result).toEqual([{}])
+// })
