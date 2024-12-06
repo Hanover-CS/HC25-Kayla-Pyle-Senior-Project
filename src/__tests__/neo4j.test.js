ebf01188833740032a007e5c1b2d09f4de54b094
Kayla Pyle <pylek25@hanover.edu>
2024-12-06 00:54:10 -0500

    add more tests Create Unit Tests #16

--- src/__tests__/neo4j.test.js
+++ src/__tests__/neo4j.test.js
@@ -1,5 +1,5 @@
 
-import { expect, test } from 'vitest'
+import { expect, test, afterEach } from 'vitest'
 import { Neo4jDriver } from '../app/lib/neo4j'
 
 /**
@@ -7,25 +7,40 @@ import { Neo4jDriver } from '../app/lib/neo4j'
  * functions of the Neo4jDriver class.
  */
 
-function clearDB() {
-    const entries = Neo4jDriver.getAllEntries()
+afterEach(async () => {
+    const entries = await Neo4jDriver.getAllEntries()
     for (let i = 0; i < entries.length; i++) {
-        Neo4jDriver.deleteEntry(entries[i].name)
+        await Neo4jDriver.deleteEntry(entries[i].name)
     }
-}
+});
 
 test('createEntry method adds Bob', async () => {
     await Neo4jDriver.createEntry('Bob')
     let result = await Neo4jDriver.readEntry('Bob')
     expect(result).toEqual([{name: 'Bob', type: 'None', text:''}])
-    clearDB()
 })
+
+test('createEntry method adds Bob with specified type', async () => {
+    await Neo4jDriver.createEntry('Bob', 'Character')
+    let result = await Neo4jDriver.readEntry('Bob')
+    expect(result).toEqual([{name: 'Bob', type: 'Character', text:''}])
+} )
 
 test('deleteEntry deletes Bob', async () => {
     await Neo4jDriver.createEntry('Bob')
     await Neo4jDriver.deleteEntry('Bob')
     let result = await Neo4jDriver.readEntry('Bob')
     expect(result).toEqual([])
+})
+
+test('deleteEntry deletes Bob and not Sally or Monica', async () => {
+    await Neo4jDriver.createEntry('Bob')
+    await Neo4jDriver.createEntry('Sally')
+    await Neo4jDriver.createEntry('Monica')
+    await Neo4jDriver.deleteEntry('Bob')
+    let result = await Neo4jDriver.getAllEntries()
+    expect(result).toEqual(expect.arrayContaining([
+        {name: 'Sally', type: 'None'}, {name: 'Monica', type: 'None'} ]))
 })
 
 test('getAllEntries returns Bob, Sally, and Monica', async () => {
@@ -35,5 +50,9 @@ test('getAllEntries returns Bob, Sally, and Monica', async () => {
     let result = await Neo4jDriver.getAllEntries()
     expect(result).toEqual(expect.arrayContaining([
         {name: 'Sally', type: 'None'}, {name: 'Monica', type: 'None'}, {name: 'Bob', type: 'None'} ]))
-    clearDB()
+})
+
+test('getAllEntries returns empty list when DB is empty', async () => {
+    let result = await Neo4jDriver.getAllEntries()
+    expect(result).toEqual([])
 })
